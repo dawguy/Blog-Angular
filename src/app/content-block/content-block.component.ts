d9df22dfb4d59c8e6ac76b964192634a17117692
dawguy <hockeyguy411@gmail.com>
2023-04-14 10:13:52 -0400

    Basic templates for posts added.

--- src/app/content-block/content-block.component.ts
+++ src/app/content-block/content-block.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-content-block',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-block.component.css']
 })
 export class ContentBlockComponent {
+  @Input() type: string;
+  @Input() lines: string[];
+  subtype: string = '';
 
+  constructor() {}
+
+  ngOnInit() {
+    if(this.type.indexOf('code-') != -1){
+      this.subtype = this.type.slice(5);
+      this.type = 'code';
+    }
+  }
 }
